60c66574d13ae2605246f9c915dc8532795580ec
Imperface <kovzhenko.ua@gmail.com>
2023-09-03 17:25:36 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,90 @@
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const refsInput = {
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+};
+const refBtnCreate = document.querySelector('button[type=submit]');
+const refForm = document.querySelector('.form');
+let inputStepIntervalId = null;
+let userInputData = {};
+refBtnCreate.setAttribute('disabled', '');
+
+const clearInput = () => {
+  refsInput.delay.value = null;
+  refsInput.step.value = null;
+  refsInput.amount.value = null;
+};
+
+clearInput();
+
+const setInputValues = () => ({
+  delayValue: Number(refsInput.delay.value),
+  stepValue: Number(refsInput.step.value),
+  amountValue: Number(refsInput.amount.value),
+});
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }
+    reject(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 }
+
+const onBtnCreateClick = e => {
+  e.preventDefault();
+  refBtnCreate.setAttribute('disabled', '');
+  const { delayValue, stepValue, amountValue } = userInputData;
+  let counter = 1;
+  setTimeout(() => {
+    createPromise(counter, delayValue)
+      .then(result => {
+        Notify.success(result);
+      })
+      .catch(result => {
+        Notify.failure(result);
+      })
+      .finally(() => {
+        counter += 1;
+      });
+  }, delayValue);
+
+  inputStepIntervalId = setInterval(() => {
+    createPromise(counter, stepValue)
+      .then(result => {
+        Notify.success(result);
+      })
+      .catch(result => {
+        Notify.failure(result);
+      })
+      .finally(() => {
+        if (counter === amountValue) {
+          clearInterval(inputStepIntervalId);
+          refBtnCreate.removeAttribute('disabled', '');
+        }
+        counter += 1;
+      });
+  }, stepValue);
+};
+const onFormInput = e => {
+  userInputData = setInputValues();
+  const { delayValue, stepValue, amountValue } = userInputData;
+  const isBackspace = e.inputType === 'deleteContentBackward';
+  const isDelete = e.inputType === 'deleteContentForward';
+
+  const patternNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  if (!(patternNumbers.includes(e.data) || isBackspace || isDelete)) {
+    Report.failure('Enter the numbers', '', 'Try again');
+    clearInput();
+    return;
+  }
+  if (delayValue && stepValue && amountValue) {
+    refBtnCreate.removeAttribute('disabled', '');
+  }
+};
+refBtnCreate.addEventListener('click', onBtnCreateClick);
+refForm.addEventListener('input', onFormInput);
